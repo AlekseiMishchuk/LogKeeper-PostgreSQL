1f7a380f6ce030bea803db47a5e13bf6e05fae55
AlekseiMishchuk <gcbkjc@gmail.com>
2024-06-13 23:23:05 +0200

    basic refactor to razor components + bootstrap

--- LogKeeperApp/LogKeeperPg/Controllers/KeeperSaveController.cs
+++ LogKeeperApp/LogKeeperPg/Controllers/KeeperSaveController.cs
@@ -1,56 +1,56 @@
-﻿using LogKeeperPg.Data;
-using LogKeeperPg.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace LogKeeperPg.Controllers;
-
-public class KeeperSaveController : Controller
-{
-    private readonly PostgresDbContext _postgresDbContext;
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly string? _token;
-
-    private const string GetValuePath = "/get-log?id={0}";
-
-    public KeeperSaveController(PostgresDbContext postgresDbContext, IHttpContextAccessor httpContextAccessor,  IConfiguration configuration)
-    {
-        _postgresDbContext = postgresDbContext;
-        _httpContextAccessor = httpContextAccessor;
-        _token = configuration["PassToken"];
-    }
-
-    
-
-    [HttpPost]
-    [Route("/keeper-save")]
-    public async Task<IActionResult> KeeperSave(string token, string title, string author, string project,
-        string contents)
-    {
-        if (string.IsNullOrWhiteSpace(token))
-        {
-            return StatusCode(500, "Tokens not set");
-        }
-
-        if (!string.IsNullOrWhiteSpace(_token) && token != _token)
-        {
-            return StatusCode(401, "Auth failed");
-        }
-
-        try
-        {
-            var logId = Guid.NewGuid();
-            await _postgresDbContext.AddAsync(new LogInformation(logId, title, author, project, DateTime.UtcNow, contents));
-            await _postgresDbContext.SaveChangesAsync();
-            var urlFormat = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}{GetValuePath}";
-            return StatusCode(200, new
-            {
-                id = logId,
-                url_format = urlFormat
-            });
-        }
-        catch (Exception e)
-        {
-            return StatusCode(200, e);
-        }
-    }
-}
+﻿// using LogKeeperPg.Data;
+// using LogKeeperPg.Models;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace LogKeeperPg.Controllers;
+//
+// public class KeeperSaveController : Controller
+// {
+//     private readonly PostgresDbContext _postgresDbContext;
+//     private readonly IHttpContextAccessor _httpContextAccessor;
+//     private readonly string? _token;
+//
+//     private const string GetValuePath = "/get-log?id={0}";
+//
+//     public KeeperSaveController(PostgresDbContext postgresDbContext, IHttpContextAccessor httpContextAccessor,  IConfiguration configuration)
+//     {
+//         _postgresDbContext = postgresDbContext;
+//         _httpContextAccessor = httpContextAccessor;
+//         _token = configuration["PassToken"];
+//     }
+//
+//     
+//
+//     [HttpPost]
+//     [Route("/keeper-save")]
+//     public async Task<IActionResult> KeeperSave(string token, string title, string author, string project,
+//         string contents)
+//     {
+//         if (string.IsNullOrWhiteSpace(token))
+//         {
+//             return StatusCode(500, "Tokens not set");
+//         }
+//
+//         if (!string.IsNullOrWhiteSpace(_token) && token != _token)
+//         {
+//             return StatusCode(401, "Auth failed");
+//         }
+//
+//         try
+//         {
+//             var logId = Guid.NewGuid();
+//             await _postgresDbContext.AddAsync(new LogInformation(logId, title, author, project, DateTime.UtcNow, contents));
+//             await _postgresDbContext.SaveChangesAsync();
+//             var urlFormat = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}{GetValuePath}";
+//             return StatusCode(200, new
+//             {
+//                 id = logId,
+//                 url_format = urlFormat
+//             });
+//         }
+//         catch (Exception e)
+//         {
+//             return StatusCode(200, e);
+//         }
+//     }
+// }
--- LogKeeperApp/LogKeeperPg/Program.cs
+++ LogKeeperApp/LogKeeperPg/Program.cs
@@ -1,32 +1,42 @@
+using LogKeeperPg.Components;
+using LogKeeperPg.Components.Pages;
 using LogKeeperPg.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddRazorPages();
+var route = builder.Configuration["LogKeeperUrl"] ?? 
+            throw new Exception("LogKeeperUrl is not set"); 
+
+// builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorComponents(options => 
+        options.DetailedErrors = builder.Environment.IsDevelopment())
+        .AddInteractiveServerComponents();
+
 builder.Services.AddDbContext<PostgresDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PgConnection")));
+builder.Services.AddHttpClient<GetLog>(client => client.BaseAddress = new Uri(route));
+
+builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
-
+// app.UseRouting();
+app.UseAntiforgery();
 app.UseAuthorization();
 
-app.MapRazorPages();
-app.MapControllers();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+// app.MapControllers();
 
 app.Run();
